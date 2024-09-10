2b5da11b58c738768c503e3a5124236eb9071639
Dante Hahroff <dante.harhoff@elev.ga.ntig.se>
2024-09-10 13:21:29 +0200

    first commit

---
+++ Quiz/Program.cs
@@ -0,0 +1,25 @@
+
+
+using System.Threading.Tasks.Dataflow;
+
+int score = 0;
+
+string choice = "";
+
+choice = Console.ReadLine();
+
+
+Console.ReadLine();
+
+if (choice == "b")
+
+{
+
+    
+    Console.WriteLine(score);
+}
+
+Console.ReadLine();
+
+
+
